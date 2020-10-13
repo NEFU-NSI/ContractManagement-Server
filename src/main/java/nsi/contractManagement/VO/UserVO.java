47f4364422e3451f11d4df2ee756dfd9f8ecd20c
Tao <2591036017@qq.com>
2020-10-13 20:01:20 +0800

    [新增功能](1.1.1): 添加登录表单校验

--- src/main/java/nsi/contractManagement/VO/UserVO.java
+++ src/main/java/nsi/contractManagement/VO/UserVO.java
@@ -3,6 +3,9 @@ package nsi.contractManagement.VO;
 import lombok.Data;
 import nsi.contractManagement.DO.UserDO;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 /**
  * @Author: Tao
  * @Time: 2020/10/11 21:27
@@ -12,7 +15,11 @@ import nsi.contractManagement.DO.UserDO;
  */
 @Data
 public class UserVO {
+    @Email
+    @NotBlank(message = "邮箱不能为空")
     String email;
+    @NotBlank(message = "用户名不能为空")
     String name;
+    @NotBlank(message = "部门不能为空")
     String department;
 }
